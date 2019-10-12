2fb1c848cfd57ef4125cdc236e94e6dfcf926311
Nicholas Claudio <nclaudio449@gmail.com>
2019-10-12 15:25:33 -0500

    Added connection to database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 const app = express();
+
+// Connect database
+connectDatabase();
 
 app.get('/', (req, res) => res.send('http get request sent to root api endpoint'));
 
